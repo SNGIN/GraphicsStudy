09eb34b5cd8489b09141ef0096648cb68565ff6d
NUNUA <syakunage-pop@biscuit.ocn.ne.jp>
2016-07-27 06:46:50 +0900

    Sample Commit

---
+++ GLTest/g_test3/main.cpp
@@ -0,0 +1,22 @@
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "opengl32.lib")
+
+static void cleanup(void){
+}
+
+// ‰Šúİ’è
+static void init()
+{
+
+}
+
+void draw(){
+
+}
+
+
+int main(){
+
+	return 0;
+}
