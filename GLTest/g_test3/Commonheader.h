8b2dd29faf571b68304148b2ca43243a9fb955d7
NUNUA <syakunage-pop@biscuit.ocn.ne.jp>
2016-07-30 02:24:57 +0900

    GLTest delete

--- GLTest/g_test3/Commonheader.h
+++
@@ -1,9 +0,0 @@
-#pragma comment(lib, "glfw3.lib")
-#pragma comment(lib, "glew32.lib")
-#pragma comment(lib, "opengl32.lib")
-
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
-#include <vector>
-#define DEBUG
